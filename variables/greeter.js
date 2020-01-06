5bf3445cd81cdebef41788c6177ecd8885cd31c7
Conor Chung <ConorChung@gmail.com>
2020-01-06 13:47:42 -0800

    organized by chapters

---
+++ variables/greeter.js
@@ -0,0 +1,7 @@
+let greeter = name => {
+  console.log('Good Morning, ' + name + '.');
+  console.log('Good Afternoon, ' + name + '.');
+  console.log('Good Evening, ' + name + '.');
+};
+
+greeter('Conor');
