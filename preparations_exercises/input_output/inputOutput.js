e7dd31e1f7c308a333146b3a66f64c9b235676f6
Conor Chung <ConorChung@gmail.com>
2020-07-29 19:07:08 -0700

    finished input/output, created matching directorys

---
+++ preparations_exercises/input_output/inputOutput.js
@@ -0,0 +1,26 @@
+// greetings.js
+//let name = 'Jane';
+//console.log(`Good morning, ${name}!`);
+
+//personalized_greeting.js
+/*
+let rlSync = require('readline-sync');
+let name = rlSync.question("What's your name?\n");
+console.log(`Good Morning, ${name}!`);
+*/
+
+//sum_numbers.js
+/*
+let rlSync = require('readline-sync');
+let number1 = Number(rlSync.question('Enter the first number\n'));
+let number2 = Number(rlSync.question('Enter the second number\n'));
+let sum = number1 + number2;
+console.log(`The numbers ${number1} and ${number2} add to ${sum}.`);
+*/
+/*
+let rlSync = require('readline-sync');
+let exampleArray = []
+exampleArray.push(rlSync.question('What do you want on this array? \n'));
+exampleArray.push(rlSync.question('What else do you want in this array?\n'));
+console.log(exampleArray);
+*/
